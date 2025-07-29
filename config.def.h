4e301535e3e6e17c6dea5836f2955ec10b0a400d
David Elliott <david@starjump.org>
2025-07-29 20:29:58 +0100

    cool-autostart

--- config.def.h
+++ config.def.h
@@ -32,6 +32,13 @@ static const unsigned int alphas[][3]      = {
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
+static const char *const autostart[] = {
+	"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1", NULL,
+	"nitrogen", "--restore", NULL,
+	"picom", "-b", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
